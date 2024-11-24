fix: resize and normalization

code/deploy/api/app.py
@@ -30,7 +30,7 @@ try:
     model = get_peft_model(base_model, lora_config)
     model = nn.DataParallel(model)
 
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'), weights_only=True))
     model.eval()
 
 except Exception as e:
@@ -46,10 +46,13 @@ class InputData(BaseModel):
 def predict(input_data: InputData):
 
     img_array = np.array(input_data.image, dtype=np.float32)
-    img_tensor = torch.tensor(img_array).unsqueeze(0)
+    image_tensor = torch.tensor(img_array)
+
+    if image_tensor.ndim == 3:
+        image_tensor = image_tensor.unsqueeze(0)
 
     with torch.no_grad():
-        outputs = model(img_tensor)
+        outputs = model(image_tensor)
         predictions = torch.argmax(outputs.logits, dim=-1)
 
     return {"prediction": int(predictions.item())}

code/deploy/app/streamlit_app.py
@@ -3,6 +3,9 @@ import requests
 import numpy as np
 import cv2
 import torch
+import json
+import pandas as pd
+import os
 
 def crop_black_lines(image: np.ndarray) -> np.ndarray:
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -19,11 +22,30 @@ def crop_black_lines(image: np.ndarray) -> np.ndarray:
 def input_prep(image: np.ndarray):
 
     img_np = crop_black_lines(image)
-    img_np = cv2.resize(img_np, (224, 224), interpolation=cv2.INTER_AREA)
+    img_np = cv2.resize(img_np, (256, 256), interpolation=cv2.INTER_AREA)
     img_tensor = (
         torch.tensor(np.transpose(img_np, (2, 0, 1)), dtype=torch.float32) / 255.0
     )
+    
+    mean = torch.tensor([2.9317e-09, 2.8943e-09, 2.8340e-09])
+    std = torch.tensor([1.3166e-09, 1.3476e-09, 1.4205e-09])
+    
+    img_tensor = (img_tensor - mean[:, None, None]) / std[:, None, None]
+
     return img_tensor.numpy().tolist()
+
+data_path = os.path.join('/app/data', 'iwildcam2020_train_annotations.json')
+with open(data_path) as f:
+    data = json.load(f)
+
+annotations = pd.DataFrame.from_dict(data["annotations"])
+categories = pd.DataFrame.from_dict(data["categories"])
+
+category_mapping = {row['id']: row['name'] for _, row in categories.iterrows()}
+
+unique_classes = annotations["category_id"].unique()
+category_to_index = {category_id: index for index, category_id in enumerate(unique_classes)}
+annotations["mapped_category_id"] = annotations["category_id"].map(category_to_index)
 
 
 FASTAPI_URL = "http://fastapi:80/predict"
@@ -42,16 +64,26 @@ if st.button("Predict"):
         try:
             response = requests.post(FASTAPI_URL, json={"image": processed_input})
             response.raise_for_status()
-            prediction = response.json().get("prediction", "No prediction found")
+            prediction_index = response.json().get("prediction", "No prediction found")
+
+            category_id = annotations.loc[annotations["mapped_category_id"] == prediction_index, "category_id"].values
+
+            if category_id.size > 0:
+                category_id_value = category_id[0]
+                predicted_label = category_mapping.get(category_id_value, "Unknown label")
+            else:
+                predicted_label = "No matching category found"
 
             st.image(image, caption="Uploaded Image", use_column_width=True)
-            st.write(f"Prediction: {prediction}")
+            st.write(f"Prediction Index: {prediction_index}")
+            st.write(f"Predicted Label: {predicted_label}")
 
         except requests.exceptions.RequestException as e:
             st.error(f"Request failed: {e}")
 
         except ValueError as e:
             st.error(f"Failed to decode JSON: {e}")
+
     else:
         st.write("Please upload an image.")
         